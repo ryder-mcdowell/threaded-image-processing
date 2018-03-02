e9084765a70051ef87b7344a4b4f306d6d865041
ryder-mcdowell <ryder.mcdowell@gmail.com>
2018-03-02 15:38:41 -0800

    Fix memory leaks

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: hw4
 
 hw4: hw4.o
-	gcc -o hw4 hw4.o
+	gcc -pthread -o hw4 hw4.o
 
 hw4.o: hw4.c
 	gcc -pthread -c hw4.c
--- hw4.c
+++ hw4.c
@@ -36,11 +36,16 @@ typedef struct ThreadArgs {
 } ThreadArgs;
 
 InputArgs *processInputArgs(int argc, char **argv) {
+  //read/check input arguments and store
   if (argc < 3 || argc > 4) {
     fprintf(stderr, "usage: ./a.out num_threads option [arg]\n");
     exit(1);
   } else {
     InputArgs *input = (InputArgs *) malloc(sizeof(InputArgs));
+    if (input == NULL) {
+      perror("MALLOC ERROR");
+      exit(1);
+    }
     input->num_threads = atoi(argv[1]);
     input->option = argv[2];
     if (argc == 4) {
@@ -58,7 +63,12 @@ InputArgs *processInputArgs(int argc, char **argv) {
 ImageInfo *processImageInfo() {
   ImageInfo *image;
   image = (ImageInfo *) malloc(sizeof(ImageInfo));
+  if (image == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
 
+  //read in and store image header info
   if (scanf("%s", image->type) < 0) {
     perror("SCANF ERROR");
     exit(1);
@@ -77,17 +87,17 @@ ImageInfo *processImageInfo() {
   }
   image->rotated = FALSE;
 
-  fprintf(stderr, "Image Type: %s\n", image->type);
-  fprintf(stderr, "Image Columns: %d\n", image->columns);
-  fprintf(stderr, "Image Rows: %d\n", image->rows);
-  fprintf(stderr, "Image Max: %d\n", image->max);
-
   return image;
 }
 
 ThreadSegment *createSegments(ImageInfo *image, int threadCount) {
   ThreadSegment *segments = (ThreadSegment *) malloc(sizeof(ThreadSegment) * threadCount);
+  if (segments == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
 
+  //split rows up into segments for each thread to work on
   int segment_length = image->rows / threadCount;
   int i;
   for (i = 0; i < threadCount; i++) {
@@ -113,9 +123,14 @@ void allocatePixels(ImageInfo *image) {
       pixels[r][c] = (int *) malloc(sizeof(int) * 3);
     }
   }
+  if (pixels == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
 }
 
 void allocatePixelsRotated(ImageInfo *image) {
+  //columns and rows swapped for rotated image
   int r, c;
   pixelsRotated = (int ***) malloc(sizeof(int **) * image->columns);
   for (r = 0; r < image->columns; r++) {
@@ -124,11 +139,16 @@ void allocatePixelsRotated(ImageInfo *image) {
       pixelsRotated[r][c] = (int *) malloc(sizeof(int) * 3);
     }
   }
+  if (pixelsRotated == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
 }
 
 void processPixels(ImageInfo *image) {
   allocatePixels(image);
 
+  //read in pixels to global variable from stdin
   int r, c, p;
   for (r = 0; r < image->rows; r++) {
     for (c = 0; c < image->columns; c++) {
@@ -143,10 +163,12 @@ void processPixels(ImageInfo *image) {
 }
 
 void outputPixels(ImageInfo *image) {
+  //output image header
   printf("%s\n", image->type);
   printf("%d %d\n", image->columns, image->rows);
   printf("%d\n", image->max);
 
+  //output image pixels
   int r, c;
   for (r = 0; r < image->rows; r++) {
     for (c = 0; c < image->columns; c++) {
@@ -156,10 +178,13 @@ void outputPixels(ImageInfo *image) {
 }
 
 void outputPixelsRotated(ImageInfo *image) {
+  //rows and columns swapped for rotated image
+  //output image header
   printf("%s\n", image->type);
   printf("%d %d\n", image->rows, image->columns);
   printf("%d\n", image->max);
 
+  //output image pixels
   int r, c;
   for (r = 0; r < image->columns; r++) {
     for (c = 0; c < image->rows; c++) {
@@ -169,12 +194,14 @@ void outputPixelsRotated(ImageInfo *image) {
 }
 
 void *invertPixels(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int max = threadArgs->image->max;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //invert pixels
   int r, c, p;
   for (r = start; r < end; r++) {
     for (c = 0; c < columns; c++) {
@@ -187,6 +214,7 @@ void *invertPixels(void *args) {
 }
 
 void *contrastPixels(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int max = threadArgs->image->max;
@@ -194,6 +222,7 @@ void *contrastPixels(void *args) {
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //add contrast
   int r, c, p;
   for (r = start; r < end; r++) {
     for (c = 0; c < columns; c++) {
@@ -210,11 +239,13 @@ void *contrastPixels(void *args) {
 }
 
 void *redPixels(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //remove greens and blues
   int r, c, p;
   for (r = start; r < end; r++) {
     for (c = 0; c < columns; c++) {
@@ -229,11 +260,13 @@ void *redPixels(void *args) {
 }
 
 void *greenPixels(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //remove reds and blues
   int r, c, p;
   for (r = start; r < end; r++) {
     for (c = 0; c < columns; c++) {
@@ -248,11 +281,13 @@ void *greenPixels(void *args) {
 }
 
 void *bluePixels(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //remove reds and greens
   int r, c, p;
   for (r = start; r < end; r++) {
     for (c = 0; c < columns; c++) {
@@ -267,12 +302,14 @@ void *bluePixels(void *args) {
 }
 
 void *rotateRight(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int rows = threadArgs->image->rows;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //rotate right
   fprintf(stderr, "start: %d| end: %d| col: %d\n", start, end, columns);
   int r, c, p;
   for (r = start; r < end; r++) {
@@ -287,11 +324,13 @@ void *rotateRight(void *args) {
 }
 
 void *rotateLeft(void *args) {
+  //unpack
   ThreadArgs *threadArgs = (ThreadArgs *) args;
   int columns = threadArgs->image->columns;
   int start = threadArgs->segment->start;
   int end = threadArgs->segment->end;
 
+  //rotate left
   fprintf(stderr, "start: %d| end: %d| col: %d\n", start, end, columns);
   int r, c, p;
   for (r = start; r < end; r++) {
@@ -306,14 +345,26 @@ void *rotateLeft(void *args) {
 }
 
 void callThreads(void *option, InputArgs *input, ImageInfo *image) {
+  //allocate threads
   pthread_t *threads = (pthread_t *) malloc(sizeof(pthread_t) * input->num_threads);
+  if (threads == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
 
+  //segments for threads to work on (start and stop indexes)
   ThreadSegment *segments = createSegments(image, input->num_threads);
 
+  //arguments to send to option function
   ThreadArgs *args = (ThreadArgs *) malloc(sizeof(ThreadArgs));
+  if (args == NULL) {
+    perror("MALLOC ERROR");
+    exit(1);
+  }
   args->image = image;
   args->contrast = input->contrast;
 
+  //create thread, call conversion, close thread
   fprintf(stderr, "---------------------\n");
   int i;
   for (i = 0; i < input->num_threads; i++) {
@@ -321,6 +372,14 @@ void callThreads(void *option, InputArgs *input, ImageInfo *image) {
     pthread_create(&threads[i], NULL, option, (void *) args);
     pthread_join(threads[i], NULL);
   }
+
+  //free thread/segment memory
+  // for (i = 0; i < input->num_threads; i++) {
+  //   free(&threads[i]);
+  // }
+  free(threads);
+  free(segments);
+  free(args);
 }
 
 void freePixels(ImageInfo *image) {
@@ -337,6 +396,7 @@ void freePixels(ImageInfo *image) {
 }
 
 void freePixelsRotated(ImageInfo *image) {
+  //columns and rows swapped for rotated image
   int r, c;
   for (r = 0; r < image->columns; r++) {
     for (c = 0; c < image->rows; c++) {
@@ -349,15 +409,8 @@ void freePixelsRotated(ImageInfo *image) {
   free(pixelsRotated);
 }
 
-void freeMemory(ImageInfo *image, InputArgs *input) {
-  freePixels(image);
-  free(image);
-  free(input);
-}
-
 
 int main (int argc, char **argv) {
-  //error check mallocs
   //free ThreadSegment and ThreadArgs
 
   //structs
@@ -392,24 +445,18 @@ int main (int argc, char **argv) {
     callThreads(rotateLeft, input, image);
   }
 
-  //output            /try to get one output function by swapping width and height
+  //output
   if (image->rotated == FALSE) {
     outputPixels(image);
+    freePixels(image);
   } else {
     outputPixelsRotated(image);
     freePixelsRotated(image);
   }
 
-  //free
-  freeMemory(image, input);
+  //free image/input memory
+  free(image);
+  free(input);
 
   return 0;
 }
-
-
-
-
-
-// char image[100];
-// fread(&image, 1, 100, stdin);
-// fprintf(stderr, "%s\n", image);
